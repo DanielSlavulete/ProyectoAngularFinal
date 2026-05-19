16499fed5d0ecb682d86e80b8cbdf4a5496986d0
DanielSlavulete <danielslavulete@gmail.com>
2026-05-19 19:37:37 +0200

    final

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,13 +16,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/health', (req, res) => {
-  res.json({
-    status: 'ok',
-    message: 'API funcionando correctamente',
-  });
-});
-
 app.use('/api/auth', authRoutes);
 app.use('/api/boards', boardRoutes);
 app.use('/api', noteRoutes);
